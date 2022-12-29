d304631eac3ef84d47bc3a027a33a906a1463128
gephaistos <olehmaksa@gmail.com>
2022-12-29 13:14:20 +0200

    Apply code formatting

--- src/test_cdpa.py
+++ src/test_cdpa.py
@@ -87,11 +87,24 @@ def parse():
     )
 
 
-def simulate(trace_count, bit_count, share_count, experiment_count,
-    seed=None, noise=None, verbose=False, list_traces=False):
-    # Generate the traces
-    result_success_count, lsb_success_count, bit_success_count, mask = 0, 0, 0, (1 << (bit_count - 1)) - 1
+def simulate(
+    trace_count,
+    bit_count,
+    share_count,
+    experiment_count,
+    seed=None,
+    noise=None,
+    verbose=False,
+    list_traces=False,
+):
+    result_success_count, lsb_success_count, bit_success_count, mask = (
+        0,
+        0,
+        0,
+        (1 << (bit_count - 1)) - 1
+    )
     for _ in range(experiment_count):
+        # Generate the traces
         data, traces, x, y = generate_traces(trace_count, bit_count, share_count, noise, seed)
         if verbose:
             nibble_count = ((bit_count - 1) >> 2) + 1
@@ -103,7 +116,9 @@ def simulate(trace_count, bit_count, share_count, experiment_count,
         highest_bit_match = ((x_dif ^ y_dif) >> (bit_count - 1)) == 0
         x_match_count = bit_count - 1 - (x_dif & mask).bit_count()
         y_match_count = bit_count - 1 - (y_dif & mask).bit_count()
-        success = highest_bit_match and x_match_count == bit_count - 1 and y_match_count == bit_count - 1
+        success = (
+            highest_bit_match and x_match_count == bit_count - 1 and y_match_count == bit_count - 1
+        )
         result_success_count += success
         bit_success_count += highest_bit_match + x_match_count + y_match_count
         if success:
@@ -119,18 +134,34 @@ def simulate(trace_count, bit_count, share_count, experiment_count,
         data_fmt = '{{:0{:d}x}}'.format(nibble_count)
         print()
         print('Secret    values: X = ' + data_fmt.format(x) + ', Y = ' + data_fmt.format(y))
-        print('Recovered values: X = ' + data_fmt.format(res_x) + ', Y = ' + data_fmt.format(res_y))
+        print(
+            'Recovered values: X = ' + data_fmt.format(res_x) + ', Y = ' + data_fmt.format(res_y)
+        )
         cprint('Success' if success else 'Failure', 'green' if success else 'red')
 
+    return (
+        result_success_count / experiment_count * 100,
+        lsb_success_count / experiment_count / bit_count * 100,
         bit_success_count / experiment_count / (2 * bit_count - 1) * 100
+    )
 
 
 if __name__ == '__main__':
     warnings.filterwarnings('ignore', category=RuntimeWarning)
     # Parse the command line
-    trace_count, bit_count, share_count, experiment_count, seed, noise, verbose, list_traces = parse()
+    (
+        trace_count,
+        bit_count,
+        share_count,
+        experiment_count,
+        seed,
+        noise,
+        verbose,
+        list_traces,
+    ) = parse()
     result_ratio, lsb_success_ratio, bit_success_ratio = simulate(
-        trace_count, bit_count, share_count, experiment_count, seed, noise, verbose, list_traces)
+        trace_count, bit_count, share_count, experiment_count, seed, noise, verbose, list_traces
+    )
     if not verbose:
         print('{:5.2f}% correct answers'.format(result_ratio))
         print('{:5.2f}% correct least significant bits'.format(lsb_success_ratio))
