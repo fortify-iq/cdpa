8d4ebf9bb895211c0f60a852dfc293cf40b20ecf
Oleh Maks <maksymenko@fortifyiq.com>
2023-01-01 13:06:59 +0200

    Format code style (#1)
    * Fix imports
    
    * Minor code improvements
    
    * Apply code formatting
    
    * Apply doc formatting
    
    * Add dependencies list
    
    * Add copyright

--- src/cdpa_attack.py
+++ src/cdpa_attack.py
@@ -13,30 +13,45 @@ from termcolor import cprint
 
 def cdpa_attack(data, traces, bit_count, share_count, verbose, list_traces):
     """CDPA attack on a pair of secrets X, Y.
-    Parameters:
-        data - array of input words W[i]
-        traces - array of side channel leakage values, corresponding to the input words.
-            Each side leakage value is the Hamming distance between X+W[i] and Y, where
-            both X+W[i] and Y are randomly represented as an XOR of several shares.
-            A normally distributed noise may be optionally added to this Hamming distance.
-        bit_count - the bit size of the secrets X, Y and of the input words W[i]
-        share_count - the order of the moment to be used for the attack. It should
-            match the number of shares used to generate the side channel leakage data.
-        verbose - if True, outputs a short report of steps 0-7 of the attack. (The number
-            of steps is limited in order to keep reasonable line lengths.)
-        list_traces - if True, outputs a more detailed report, including the individual
-            traces and splitting of the set of all traces into subsets at steps 0-7 of
-            the attack.
-    Returns recovered values of X and Y. The attack is considered successful if the XOR
-    of the most significant bits of X and Y and all the remaining individual bits of both
-    X and Y are guessed correctly.
+
+    Parameters
+    ----------
+    data : array_like
+        Array of input words W[i].
+    traces : np.ndarray
+        Array of side channel leakage values, corresponding to the input words.
+        Each side leakage value is the Hamming distance between X+W[i] and Y, where
+        both X+W[i] and Y are randomly represented as an XOR of several shares.
+        A normally distributed noise may be optionally added to this Hamming distance.
+    bit_count : int
+        The bit size of the secrets X, Y and of the input words W[i].
+    share_count : int
+        The order of the moment to be used for the attack.
+        It should match the number of shares used to generate
+        the side channel leakage data.
+    verbose : bool
+        If `True`, outputs a short report of steps 0-7 of the attack.
+        (The number of steps is limited in order to keep reasonable line lengths.)
+    list_traces : bool
+        If `True`, outputs a more detailed report, including the individual
+        traces and splitting of the set of all traces into subsets at steps 0-7 of
+        the attack.
+
+    Returns
+    -------
+    recovered values of X and Y : tuple[int, int]
+        The attack is considered successful
+        if the XOR of the most significant bits of X and Y
+        and all the remaining individual bits of both X and Y are guessed correctly.
     """
 
     # Step 0 (two subsets)
     traces_count = traces.shape[0]
     subsets = [data & 1 == i for i in range(2)]
-    moments = [moment(traces[t], moment=share_count)
-               if share_count > 1 else np.average(traces[t]) for t in subsets]
+    moments = [
+        moment(traces[t], moment=share_count) if share_count > 1 else np.average(traces[t])
+        for t in subsets
+    ]
     moment_trace, leaps_trace = [moments], []
     # The guessed values. At step 0, guess x_0^y_0 and store it into y_0.
     # x is initialized with 0.
@@ -45,15 +60,17 @@ def cdpa_attack(data, traces, bit_count, share_count, verbose, list_traces):
     # The remaining steps. i == step_index - 1
     for i in range(bit_count - 1):
         subsets = [((data + x) >> i) & 3 == j for j in range(4)]
-        moments = [moment(traces[t], moment=share_count)
-                   if share_count > 1 else np.average(traces[t]) for t in subsets]
+        moments = [
+            moment(traces[t], moment=share_count) if share_count > 1 else np.average(traces[t])
+            for t in subsets
+        ]
         moment_trace.append(moments)
         leaps = (
             moments[0] - moments[1] - moments[2] + moments[3],
             moments[1] - moments[2] - moments[3] + moments[0],
         )
         leaps_trace.append(leaps)
-        bit0 = int(abs(leaps[0]) > abs(leaps[1]))  # The position of the d2L maximal by absolute value
+        bit0 = int(abs(leaps[0]) > abs(leaps[1]))  # The position of the d2L maximal by abs value
         bit1 = int((leaps[not bit0] < 0) ^ (share_count & 1))  # Its sign
         case = bit0 + (bit1 << 1)
         x ^= bit0 << i  # Store the guessed x_i
@@ -61,22 +78,28 @@ def cdpa_attack(data, traces, bit_count, share_count, verbose, list_traces):
 
     if verbose or list_traces:
         nibble_count = ((bit_count - 1) >> 2) + 1
-        data_fmt = '{' + ':0{:d}x'.format(nibble_count) + '}'
+        data_fmt = '{{:0{:d}x}}'.format(nibble_count)
         space = ' ' * (nibble_count - 1) if list_traces else ' '
         if bit_count > 8:
             bit_count = 8
         print(space + ' ' * 7
               + '  '.join('Step {}'.format(i) for i in range(bit_count)))
         if list_traces:
-            print(space + 'W   HD    W&1 '
-                  + ' '.join('((W+{:2x})'.format(x & ((1 << i) - 1)) for i in range(bit_count - 1)))
-            print(space + ' ' * 15
-                  + '  '.join('>>{})&3'.format(i) for i in range(bit_count - 1)))
+            print(
+                space
+                + 'W   HD    W&1 '
+                + ' '.join('((W+{:2x})'.format(x & ((1 << i) - 1)) for i in range(bit_count - 1))
+            )
+            print(space + ' ' * 15 + '  '.join('>>{})&3'.format(i) for i in range(bit_count - 1)))
             for i in range(traces_count):
                 print((data_fmt + '   {:2d}  ').format(data[i], traces[i]), end='')
                 print('   M{}      '.format(int(data[i]) & 1), end='')
-                print((' ' * 6).join('M{}'.format(
-                    ((int(data[i]) + (x & ((1 << j) - 1))) >> j) & 3) for j in range(bit_count - 1)))
+                print(
+                    (' ' * 6).join(
+                        'M{}'.format(((int(data[i]) + (x & ((1 << j) - 1))) >> j) & 3)
+                        for j in range(bit_count - 1)
+                    )
+                )
             print()
         for j in range(4):
             print('L(M{})   '.format(j) + space, end='')
@@ -94,7 +117,9 @@ def cdpa_attack(data, traces, bit_count, share_count, verbose, list_traces):
                 print(' ' * 5, end='')
             else:
                 dif = moment_trace[0][1] - moment_trace[0][0]
-                cprint('{:5.2f}'.format(dif), 'white', 'on_blue' if dif > 0 else 'on_yellow', end='')
+                cprint(
+                    '{:5.2f}'.format(dif), 'white', 'on_blue' if dif > 0 else 'on_yellow', end=''
+                )
             for i in range(1, bit_count):
                 print('   {:5.2f}'.format(moment_trace[i][j + 2] - moment_trace[i][j]), end='')
             print()
@@ -105,8 +130,12 @@ def cdpa_attack(data, traces, bit_count, share_count, verbose, list_traces):
                 cur = leaps_trace[i][j]
                 print('   ', end='')
                 if abs(cur) > abs(leaps_trace[i][1 - j]):
-                    cprint('{:5.2f}'.format(cur),
-                           'white', 'on_blue' if cur < 0 else 'on_yellow', end='')
+                    cprint(
+                        '{:5.2f}'.format(cur),
+                        'white',
+                        'on_blue' if cur < 0 else 'on_yellow',
+                        end=''
+                    )
                 else:
                     print('{:5.2f}'.format(cur), end='')
             print()
@@ -132,5 +161,6 @@ def cdpa_attack(data, traces, bit_count, share_count, verbose, list_traces):
         print()
 
     # The most significant bit of x is always 0.
-    # The most significant bit of y contains the guessed XOR of the most significant bits of x and y.
+    # The most significant bit of y contains the guessed XOR
+    # of the most significant bits of x and y.
     return x, y
--- src/test_cdpa.py
+++ src/test_cdpa.py
@@ -8,8 +8,11 @@
 
 import warnings
 import argparse
-from CdpaAttack import cdpa_attack
-from CdpaTraceGeneration import generate_traces
+
+from termcolor import cprint
+
+from cdpa_attack import cdpa_attack
+from cdpa_trace_generation import generate_traces
 
 
 def parse():
@@ -72,6 +75,7 @@ def parse():
     args.verbose |= args.list_of_traces
     assert not args.verbose or args.experiment_count == 1, \
         '"-v" and "-l" are permitted only if the experiment count is 1 ("-e 1" or default)'
+
     return (
         args.trace_count,
         args.bit_count,
@@ -84,15 +88,28 @@ def parse():
     )
 
 
-def simulate(trace_count, bit_count, share_count, experiment_count,
-    seed=None, noise=None, verbose=False, list_traces=False):
-    # Generate the traces
-    result_success_count, lsb_success_count, bit_success_count, mask = 0, 0, 0, (1 << (bit_count - 1)) - 1
-    for i in range(experiment_count):
+def simulate(
+    trace_count,
+    bit_count,
+    share_count,
+    experiment_count,
+    seed=None,
+    noise=None,
+    verbose=False,
+    list_traces=False,
+):
+    result_success_count, lsb_success_count, bit_success_count, mask = (
+        0,
+        0,
+        0,
+        (1 << (bit_count - 1)) - 1
+    )
+    for _ in range(experiment_count):
+        # Generate the traces
         data, traces, x, y = generate_traces(trace_count, bit_count, share_count, noise, seed)
         if verbose:
             nibble_count = ((bit_count - 1) >> 2) + 1
-            data_fmt = '{' + ':0{:d}x'.format(nibble_count) + '}'
+            data_fmt = '{{:0{:d}x}}'.format(nibble_count)
             print('Secret values: X = ' + data_fmt.format(x) + ', Y = ' + data_fmt.format(y))
             print()
         res_x, res_y = cdpa_attack(data, traces, bit_count, share_count, verbose, list_traces)
@@ -100,7 +117,9 @@ def simulate(trace_count, bit_count, share_count, experiment_count,
         highest_bit_match = ((x_dif ^ y_dif) >> (bit_count - 1)) == 0
         x_match_count = bit_count - 1 - (x_dif & mask).bit_count()
         y_match_count = bit_count - 1 - (y_dif & mask).bit_count()
-        success = highest_bit_match and x_match_count == bit_count - 1 and y_match_count == bit_count - 1
+        success = (
+            highest_bit_match and x_match_count == bit_count - 1 and y_match_count == bit_count - 1
+        )
         result_success_count += success
         bit_success_count += highest_bit_match + x_match_count + y_match_count
         if success:
@@ -113,22 +132,37 @@ def simulate(trace_count, bit_count, share_count, experiment_count,
 
     if verbose:
         nibble_count = ((bit_count - 1) >> 2) + 1
-        data_fmt = '{' + ':0{:d}x'.format(nibble_count) + '}'
+        data_fmt = '{{:0{:d}x}}'.format(nibble_count)
         print()
         print('Secret    values: X = ' + data_fmt.format(x) + ', Y = ' + data_fmt.format(y))
-        print('Recovered values: X = ' + data_fmt.format(res_x) + ', Y = ' + data_fmt.format(res_y))
-        print('Success' if success else 'Failure')
-    return result_success_count / experiment_count * 100, \
-        lsb_success_count / experiment_count / bit_count * 100, \
+        print(
+            'Recovered values: X = ' + data_fmt.format(res_x) + ', Y = ' + data_fmt.format(res_y)
+        )
+        cprint('Success' if success else 'Failure', 'green' if success else 'red')
+
+    return (
+        result_success_count / experiment_count * 100,
+        lsb_success_count / experiment_count / bit_count * 100,
         bit_success_count / experiment_count / (2 * bit_count - 1) * 100
+    )
 
 
 if __name__ == '__main__':
     warnings.filterwarnings('ignore', category=RuntimeWarning)
     # Parse the command line
-    trace_count, bit_count, share_count, experiment_count, seed, noise, verbose, list_traces = parse()
+    (
+        trace_count,
+        bit_count,
+        share_count,
+        experiment_count,
+        seed,
+        noise,
+        verbose,
+        list_traces,
+    ) = parse()
     result_ratio, lsb_success_ratio, bit_success_ratio = simulate(
-        trace_count, bit_count, share_count, experiment_count, seed, noise, verbose, list_traces)
+        trace_count, bit_count, share_count, experiment_count, seed, noise, verbose, list_traces
+    )
     if not verbose:
         print('{:5.2f}% correct answers'.format(result_ratio))
         print('{:5.2f}% correct least significant bits'.format(lsb_success_ratio))
